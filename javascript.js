c3139c51deedd1a251b90b14ef624b4e6cc1b572
UnnamedMinecrafter <UnnamedMinecrafter@users.noreply.github.com>
2017-09-01 19:12:40 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,15 +2,17 @@ var thumbnails = document.getElementsByTagName("ytd-thumbnail");
 for(var i=0; i<thumbnails.length; i++) {
   var thumb = thumbnails[i];
   var thumbLink = thumb.getElementsByTagName("a")[0];
-  var href = thumbLink.href;
-  
-  var code = href.slice(32,href.length);
-  
-  var iframe = document.createElement("iframe");
-  iframe.width = 210;
-  iframe.height = 118;
-  iframe.src = "https://www.youtube.com/embed/"+code;
-  
-  thumb.innerHTML = "";
-  thumb.appendChild(iframe);
+  if(thumbLink!=undefined) {
+    var href = thumbLink.href;
+
+    var code = href.slice(32,href.length);
+
+    var iframe = document.createElement("iframe");
+    iframe.width = 210;
+    iframe.height = 118;
+    iframe.src = "https://www.youtube.com/embed/"+code;
+
+    thumb.innerHTML = "";
+    thumb.appendChild(iframe);
+  }
 }
