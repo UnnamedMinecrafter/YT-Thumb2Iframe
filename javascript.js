e51e5d9a7b5152ad9474a6f2639dd0c00e476453
UnnamedMinecrafter <UnnamedMinecrafter@users.noreply.github.com>
2017-09-01 20:08:52 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,20 +1,25 @@
 //javascript:(function(){var url='//rawgithub.com/UnnamedMinecrafter/YT-Thumb2Iframe/master/javascript.js';document.head.appendChild(document.createElement('script')).src=url;})();
 
-var thumbnails = document.getElementsByTagName("ytd-thumbnail");
-for(var i=0; i<thumbnails.length; i++) {
-  var thumb = thumbnails[i];
-  var thumbLink = thumb.getElementsByTagName("a")[0];
-  if(thumbLink!=undefined) {
-    var href = thumbLink.href;
+function main() {
+  setTimeout(main,500);
+  var thumbnails = document.getElementsByTagName("ytd-thumbnail");
+  for(var i=0; i<thumbnails.length; i++) {
+    var thumb = thumbnails[i];
+    var thumbLink = thumb.getElementsByTagName("a")[0];
+    if(thumbLink!=undefined) {
+      var href = thumbLink.href;
 
-    var code = href.slice(32,href.length);
+      var code = href.slice(32,href.length);
 
-    var iframe = document.createElement("iframe");
-    iframe.width = 210;
-    iframe.height = 118;
-    iframe.src = "https://www.youtube.com/embed/"+code;
+      var iframe = document.createElement("iframe");
+      iframe.width = 210;
+      iframe.height = 118;
+      iframe.src = "https://www.youtube.com/embed/"+code;
 
-    thumb.innerHTML = "";
-    thumb.appendChild(iframe);
+      thumb.innerHTML = "";
+      thumb.appendChild(iframe);
+    }
   }
 }
+main();
+
