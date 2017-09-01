dff98ea6660499db4272b024f86125a7d6901901
UnnamedMinecrafter <UnnamedMinecrafter@users.noreply.github.com>
2017-09-01 18:25:34 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 var thumbnails = document.getElementsByTagName("ytd-thumbnail");
-for(var i=0; i<thumbnails.length&&i<3; i++) {
+for(var i=0; i<thumbnails.length; i++) {
   var thumbLink = thumbnails[i];
   var href = thumbLink.href;
-  thumbLink.innerHTML = "test";
+  thumbLink.innerHTML = href;
 }
